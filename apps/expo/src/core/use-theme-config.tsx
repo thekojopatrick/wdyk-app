974473774f3c7bc0c6be1970b57d048ba7be90a2
Kojo Patrick <patrickasare84@gmail.com>
2024-05-29 10:58:32 +0000

    setup theme config

---
+++ apps/expo/src/core/use-theme-config.tsx
@@ -0,0 +1,36 @@
+import type { Theme } from "@react-navigation/native";
+import colors from "@/theme/colors";
+import {
+  DarkTheme as _DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
+import { useColorScheme } from "nativewind";
+
+const DarkTheme: Theme = {
+  ..._DarkTheme,
+  colors: {
+    ..._DarkTheme.colors,
+    primary: colors.primary[200],
+    background: colors.charcoal[950],
+    text: colors.charcoal[100],
+    border: colors.charcoal[500],
+    card: colors.charcoal[850],
+  },
+};
+
+const LightTheme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: colors.primary[400],
+    background: colors.white,
+  },
+};
+
+export function useThemeConfig() {
+  const { colorScheme } = useColorScheme();
+
+  if (colorScheme === "dark") return DarkTheme;
+
+  return LightTheme;
+}
