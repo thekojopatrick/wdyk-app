34236f23c0aec8e50ad889d58bd47ec37fe24a32
Kojo Patrick <patrickasare84@gmail.com>
2024-05-30 10:05:33 +0000

    Moved CreatePost function from post/index.tsx to post/add-post.tsx

--- apps/expo/src/app/post/add-post.tsx
+++ apps/expo/src/app/post/add-post.tsx
@@ -14,6 +14,64 @@ const schema = z.object({
 
 type FormType = z.infer<typeof schema>;
 
+// function CreatePost() {
+//   const utils = api.useUtils();
+
+//   const [title, setTitle] = useState("");
+//   const [content, setContent] = useState("");
+
+//   const { mutate, error } = api.post.create.useMutation({
+//     async onSuccess() {
+//       setTitle("");
+//       setContent("");
+//       await utils.post.all.invalidate();
+//     },
+//   });
+
+//   return (
+//     <View className="mt-4 flex gap-2">
+//       <TextInput
+//         className=" items-center rounded-md border border-input bg-background px-3 py-4 text-lg leading-[1.25] text-foreground"
+//         value={title}
+//         onChangeText={setTitle}
+//         placeholder="Title"
+//       />
+//       {error?.data?.zodError?.fieldErrors.title && (
+//         <Text className="mb-2 text-destructive">
+//           {error.data.zodError.fieldErrors.title}
+//         </Text>
+//       )}
+//       <TextInput
+//         className="items-center rounded-md border border-input bg-background px-3 py-4  text-lg leading-[1.25] text-foreground"
+//         value={content}
+//         onChangeText={setContent}
+//         placeholder="Content"
+//       />
+//       {error?.data?.zodError?.fieldErrors.content && (
+//         <Text className="mb-2 text-destructive">
+//           {error.data.zodError.fieldErrors.content}
+//         </Text>
+//       )}
+//       <Pressable
+//         className="flex items-center rounded bg-primary p-2 py-4"
+//         onPress={() => {
+//           mutate({
+//             title,
+//             content,
+//           });
+//         }}
+//       >
+//         <Text className="text-primary-foreground">Create</Text>
+//       </Pressable>
+//       {error?.data?.code === "UNAUTHORIZED" && (
+//         <Text className="mt-2 text-destructive">
+//           You need to be logged in to create a post
+//         </Text>
+//       )}
+//     </View>
+//   );
+// }
+
 export default function AddPost() {
   const { control, handleSubmit } = useForm<FormType>({
     resolver: zodResolver(schema),
--- apps/expo/src/app/post/index.tsx
+++ apps/expo/src/app/post/index.tsx
@@ -36,64 +36,6 @@ function PostCard(props: {
   );
 }
 
-// function CreatePost() {
-//   const utils = api.useUtils();
-
-//   const [title, setTitle] = useState("");
-//   const [content, setContent] = useState("");
-
-//   const { mutate, error } = api.post.create.useMutation({
-//     async onSuccess() {
-//       setTitle("");
-//       setContent("");
-//       await utils.post.all.invalidate();
-//     },
-//   });
-
-//   return (
-//     <View className="mt-4 flex gap-2">
-//       <TextInput
-//         className=" items-center rounded-md border border-input bg-background px-3 py-4 text-lg leading-[1.25] text-foreground"
-//         value={title}
-//         onChangeText={setTitle}
-//         placeholder="Title"
-//       />
-//       {error?.data?.zodError?.fieldErrors.title && (
-//         <Text className="mb-2 text-destructive">
-//           {error.data.zodError.fieldErrors.title}
-//         </Text>
-//       )}
-//       <TextInput
-//         className="items-center rounded-md border border-input bg-background px-3 py-4  text-lg leading-[1.25] text-foreground"
-//         value={content}
-//         onChangeText={setContent}
-//         placeholder="Content"
-//       />
-//       {error?.data?.zodError?.fieldErrors.content && (
-//         <Text className="mb-2 text-destructive">
-//           {error.data.zodError.fieldErrors.content}
-//         </Text>
-//       )}
-//       <Pressable
-//         className="flex items-center rounded bg-primary p-2 py-4"
-//         onPress={() => {
-//           mutate({
-//             title,
-//             content,
-//           });
-//         }}
-//       >
-//         <Text className="text-primary-foreground">Create</Text>
-//       </Pressable>
-//       {error?.data?.code === "UNAUTHORIZED" && (
-//         <Text className="mt-2 text-destructive">
-//           You need to be logged in to create a post
-//         </Text>
-//       )}
-//     </View>
-//   );
-// }
-
 export default function Index() {
   const utils = api.useUtils();
 
