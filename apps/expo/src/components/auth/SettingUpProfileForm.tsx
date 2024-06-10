c73fd0c35147f191da65dcecf2305ed76638cd09
Kojo Patrick <patrickasare84@gmail.com>
2024-06-10 17:18:37 +0000

    [wip] setting profile

--- apps/expo/src/components/auth/SettingUpProfileForm.tsx
+++ apps/expo/src/components/auth/SettingUpProfileForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { ControlledInput, ThemeText } from "@/ui";
+import { Adinkrahene, Sankofa } from "@/ui/icons/adinkra-symbols";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -17,21 +18,39 @@ const SettingUpProfileForm = () => {
 
   return (
     <View>
-      <View className="mb-5 gap-2">
+      <View className="mb-6 gap-2">
         <ThemeText variant="title1" testID="form-title" className="font-bold">
           Complete your profile
         </ThemeText>
-        <ThemeText testID="form-description" className="pb-6">
-          Personalize your wordy journey.
+        <ThemeText
+          testID="form-description"
+          className="font-normal text-neutral-500"
+        >
+          Personalize your profile to stand out.
         </ThemeText>
       </View>
-      <ControlledInput
-        testID="username"
-        control={control}
-        name="username"
-        label="Username"
-        placeholder="@username"
-      />
+      <View className="mb-3">
+        <ControlledInput
+          testID="username"
+          control={control}
+          name="username"
+          label="Username"
+          placeholder="@username"
+        />
+      </View>
+      <View>
+        <ThemeText variant="subhead" testID="form-label" className="">
+          Pick Your Wordy Persona!
+        </ThemeText>
+        <View className="mt-3 flex flex-row flex-wrap gap-2">
+          <View className="h-24 w-24 items-center justify-center rounded-xl bg-neutral-100 p-4">
+            <Adinkrahene />
+          </View>
+          <View className="h-24 w-24 items-center justify-center rounded-xl bg-neutral-100 p-4">
+            <Sankofa />
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
