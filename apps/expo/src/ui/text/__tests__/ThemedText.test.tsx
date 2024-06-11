26b485b030bca7d32744f9e9dcdbcec7fd9531cc
Kojo Patrick <patrickasare84@gmail.com>
2024-06-11 14:37:29 +0000

    Updated imports and tsconfig.json settings in ThemedText.test.tsx

--- apps/expo/src/ui/text/__tests__/ThemedText.test.tsx
+++ apps/expo/src/ui/text/__tests__/ThemedText.test.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import * as React from "react";
+import renderer from "react-test-renderer";
 
-import { ThemeText } from '../ThemeText';
-import renderer from 'react-test-renderer';
+import { ThemeText } from "../ThemeText";
 
 it(`renders correctly`, () => {
   const tree = renderer.create(<ThemeText>Snapshot test!</ThemeText>).toJSON();
