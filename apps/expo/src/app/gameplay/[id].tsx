99b542af696a37512425940851cb023ba083790e
Kojo Patrick <patrickasare84@gmail.com>
2024-06-14 15:37:39 +0000

    Refactor Gameplay component: removed hardcoded styles, added GamePlayHeader component, and updated imports

--- apps/expo/src/app/gameplay/[id].tsx
+++ apps/expo/src/app/gameplay/[id].tsx
@@ -1,21 +1,20 @@
 import { SafeAreaView, Text, View } from "react-native";
 import { Stack, useGlobalSearchParams } from "expo-router";
 
+import GamePlayHeader from "@/components/header/GamePlayHeader";
 import { api } from "@/utils/api";
+import { colors } from "@/theme";
 
 export default function Gameplay() {
   const { id } = useGlobalSearchParams();
   if (!id || typeof id !== "string") throw new Error("unreachable");
 
   return (
-    <SafeAreaView className="bg-background">
-      <Stack.Screen options={{ title: "Wordplay" }} />
-      <View className="h-full w-full p-4">
-        <Text className="py-2 text-3xl font-bold text-primary">
-          {"Wordplay"}
-        </Text>
-        <Text className="py-4 text-foreground">{"Body"}</Text>
-      </View>
+    <SafeAreaView style={{ backgroundColor: colors.primary[600] }}>
+      <Stack.Screen
+        options={{ header: () => <GamePlayHeader title="Wordplay" /> }}
+      />
+      <View className="h-full w-full p-4"></View>
     </SafeAreaView>
   );
 }
---
+++ apps/expo/src/components/header/GamePlayHeader.tsx
@@ -0,0 +1,73 @@
+import { AntDesign, Ionicons, SimpleLineIcons } from "@expo/vector-icons";
+import { Button, ThemedText } from "@/ui";
+import { NotificationIcon, Settings as SettingsIcon } from "@/ui/icons";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { colors } from "@/theme";
+import { useRouter } from "expo-router";
+
+interface GamePlayHeaderProps {
+  title?: string;
+  titleVariant?: "largeTitle" | "title1" | "title2";
+  children?: React.ReactNode;
+}
+
+const GamePlayHeader: React.FC<GamePlayHeaderProps> = ({ title, children }) => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Button
+          size="icon"
+          style={{ backgroundColor: colors.secondary[200] }}
+          className="h-8 w-8 p-2"
+          onPress={() => router.back()}
+        >
+          <AntDesign name="close" size={16} color="black" />
+        </Button>
+        <ThemedText
+          variant={"title3"}
+          className="font-semibold uppercase text-white"
+        >
+          {title}
+        </ThemedText>
+        <View className="flex flex-row items-center gap-2">
+          <SimpleLineIcons
+            name="clock"
+            size={16}
+            color={colors.secondary[200]}
+          />
+          <ThemedText
+            variant="footnote"
+            style={{ color: colors.secondary[200] }}
+            className="font-bold"
+          >
+            2:00
+          </ThemedText>
+        </View>
+        {children}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default GamePlayHeader;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.primary[600],
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  container: {
+    flexDirection: "row",
+    height: 60,
+    gap: 20,
+    backgroundColor: colors.primary[600],
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+});
