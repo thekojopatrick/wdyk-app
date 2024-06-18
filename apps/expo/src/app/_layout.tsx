5076e4d14cb92b8c990111148ac8753c87fc4e64
Kojo Patrick <patrickasare84@gmail.com>
2024-06-18 16:48:36 +0000

    Swap order of 'splash' and 'index' screens in RootLayout

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout() {
   return (
     <Providers>
       <Stack initialRouteName="splash">
+        <Stack.Screen name="splash" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="splash" options={{ headerShown: false }} />
         <Stack.Screen name="onboarding" options={{ headerShown: false }} />
       </Stack>
       <StatusBar />
