3d6d84b59f6f5da31cd201f6b489b6d9d26b6061
Kojo Patrick <patrickasare84@gmail.com>
2024-06-24 20:24:06 +0000

    Refactor i18n initialization and storage functions

--- apps/expo/src/core/i18n/index.tsx
+++ apps/expo/src/core/i18n/index.tsx
@@ -1,5 +1,5 @@
 import { I18nManager } from "react-native";
-import { locale } from "expo-localization";
+// import { locale } from "expo-localization";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
@@ -8,17 +8,21 @@ import { getLanguage } from "./utils";
 
 export * from "./utils";
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: getLanguage() || locale, // TODO: if you are not supporting multiple languages or languages with multiple directions you can set the default value to `en`
-  fallbackLng: "en",
-  compatibilityJSON: "v3", // By default React Native projects does not support Intl
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: getLanguage() ?? "en",
+    fallbackLng: "en",
+    compatibilityJSON: "v3",
 
-  // allows integrating dynamic values into translations.
-  interpolation: {
-    escapeValue: false, // escape passed in values to avoid XSS injections
-  },
-});
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+  .catch((err) => {
+    console.error("Failed to initialize i18n", err);
+  });
 
 // Is it a RTL language?
 export const isRTL: boolean = i18n.dir() === "rtl";
--- apps/expo/src/core/storage.tsx
+++ apps/expo/src/core/storage.tsx
@@ -2,15 +2,15 @@ import { MMKV } from "react-native-mmkv";
 
 export const storage = new MMKV();
 
-export function getItem<T>(key: string): T {
+export function getItem<T>(key: string): T | null {
   const value = storage.getString(key);
-  return value ? JSON.parse(value) || null : null;
+  return value ? (JSON.parse(value) as T) : null;
 }
 
-export async function setItem<T>(key: string, value: T) {
+export function setItem<T>(key: string, value: T) {
   storage.set(key, JSON.stringify(value));
 }
 
-export async function removeItem(key: string) {
+export function removeItem(key: string) {
   storage.delete(key);
 }
